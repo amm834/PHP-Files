72f687b820311c898112b757089a87aaf3534fb7
Aung Myat Moe <65492233+amm834@users.noreply.github.com>
2020-09-12 22:12:19 +0630

    Update Core.php

--- Core.php
+++ Core.php
@@ -1,40 +1,19 @@
 <?php
-
-class Core {
-  private $className = "Home";
-  private $methodName = "index";
-  private $params = [];
-
-  function __construct() {
-    $this->getUrl();
-  }
-  
-  function getUrl() {
-    $url = isset($_GET['url']) ? rtrim($_GET['url'], '/') : '';
-    
-    $url = explode('/', $url);
-    if (!empty($url[0])) {
-      if (file_exists("../app/controllers/".ucfirst($url[0]).".php")) {
-        $this->className = $url[0];
-        unset($url[0]);
+    if (empty($type)) {
+      switch ($value) {
+        case is_int($value):
+          $type = PDO::PARAM_INT;
+          break;
+        case is_bool($value):
+          $type = PDO::PARAM_BOOL;
+          break;
+        case is_null($value):
+          $type = PDO::PARAM_NULL;
+          break;
+        default:
+          $type = PDO::PARAM_STR;
+          break;
       }
     }
-    require_once "../app/controllers/".ucfirst($this->className).".php";
-  $this->className =  new $this->className;
-    if (!empty($url[1])) {
-      if (method_exists($this->className, $this->methodName)) {
-        $this->methodName = $url[1];
-        unset($url[1]);
-      }
-    }
-    $this->params = !empty($url) ? array_values($url) : [];
-
-    call_user_func([$this->className, $this->methodName], $this->params);
-
-  }
-}
-
-
-
 
 ?>
